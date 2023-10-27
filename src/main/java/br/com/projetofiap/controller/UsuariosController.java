3a21bb219635c65f31585773ae95539daf619816
Rafael Teixeira <rafael.teixeira@libertyti.com.br>
2023-10-27 00:23:25 -0300

    :sparkles: Incluíndo swagger e ajustando a documentação

--- src/main/java/br/com/projetofiap/controller/UsuariosController.java
+++ src/main/java/br/com/projetofiap/controller/UsuariosController.java
@@ -2,8 +2,15 @@ package br.com.projetofiap.controller;
 
 import br.com.projetofiap.dto.UsuariosDTO;
 import br.com.projetofiap.service.UsuariosService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,36 +22,52 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
-
 @RestController
 @RequestMapping("/usuarios")
 @RequiredArgsConstructor
+@Tag(name = "1 - Usuário", description = "Serviços disponíveis ao usuário")
 public class UsuariosController {
 
     private final UsuariosService service;
 
     @PostMapping
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Sucesso")}
+    )
+    @Operation(summary = "Serviço responsável por cadastrar um novo usuário")
     public ResponseEntity<UsuariosDTO> gravar(@RequestBody UsuariosDTO usuarioDTO) {
         var usuario = this.service.gravar(usuarioDTO);
         var location = ServletUriComponentsBuilder.fromCurrentRequest()
-                                                  .path("/{id}")
-                                                  .buildAndExpand(usuario.id()).toUri();
+                .path("/{id}")
+                .buildAndExpand(usuario.id()).toUri();
         return ResponseEntity.created(location).build();
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Serviço responsável por atualizar os dados do usuário")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200",
+            description = "Usuário atualizado", content = {
+            @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = UsuariosDTO.class))}),
+            @ApiResponse(responseCode = "404", description = "Registro não encontrado")})
     public ResponseEntity<UsuariosDTO> atualizar(@RequestBody UsuariosDTO usuarioDTO, @PathVariable("id") Integer id) {
         return new ResponseEntity<>(this.service.atualizar(usuarioDTO, id), HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Serviço responsável por deletar um novo usuário")
+    @ApiResponses(value = {@ApiResponse(responseCode = "204", description = "Usuário excluído"),
+            @ApiResponse(responseCode = "404", description = "Registro não encontrado")})
     public ResponseEntity<Void> deletar(@PathVariable("id") Integer id) {
         this.service.deletar(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Serviço responsável por obter usuário por ID")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", content = {
+            @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = UsuariosDTO.class))}),
+            @ApiResponse(responseCode = "404", description = "Registro não encontrado")})
     public ResponseEntity<UsuariosDTO> obterPorId(@PathVariable("id") Integer id) {
         var usuario = this.service.obterUsuarioPorId(id);
         return new ResponseEntity<>(usuario, HttpStatus.OK);
